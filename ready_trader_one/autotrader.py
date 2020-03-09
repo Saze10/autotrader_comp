77657d3578d807f344f02a1eba316439b8f35243
Saze10 <saze@protonmail.com>
2020-03-09 16:14:22 +1100

    commit 5

--- ready_trader_one/autotrader.py
+++ ready_trader_one/autotrader.py
@@ -34,74 +34,94 @@ class AutoTrader(BaseAutoTrader):
         prices are reported along with the volume available at each of those
         price levels.
         """
-				
 			
 
-			
+# Entry containing ask and bid prices for given instrument
+		new_entry = {
+			"ask": [],
+			"bid": []
+		}
 
-        # Entry containing ask and bid prices for given instrument
-        new_entry = {
-            "ask": [],
-            "bid": []
-            }
 
-        
-        for i in range(5):
-            # Entry containing volume and price for given ask/bid
-            new_ask_data = {
-                "volume": ask_volume[i],
-                "price": ask_prices[i]
-            }
-            
-            new_bid_data = {
-                "volume": bid_volume[i],
-                "price": bid_prices[i]
-            }
+		for i in range(5):
+# Entry containing volume and price for given ask/bid
+		new_ask_data = {
+			"volume": ask_volume[i],
+			"price": ask_prices[i]
+		}
 
-            # Append data to corresponding list within entry dictionary
-            new_entry["ask"].append(new_ask_data)
-            new_entry["bid"].append(new_bid_data)
+		new_bid_data = {
+			"volume": bid_volume[i],
+			"price": bid_prices[i]
+		}
 
-        # Add entry to corresponding instrument dictionary
-        if instrument = Instrument.ETF:
-            etf_history[str(sequence_number)] = new_entry
-        elif instrument = Instrument.FUTURE:
-            future_history[str(sequence_number)] = new_entry
+	# Append data to corresponding list within entry dictionary
+		new_entry["ask"].append(new_ask_data)
+		new_entry["bid"].append(new_bid_data)
+
+	# Add entry to corresponding instrument dictionary
+		if instrument == Instrument.ETF:
+		etf_history[str(sequence_number)] = new_entry
+		elif instrument == Instrument.FUTURE:
+		future_history[str(sequence_number)] = new_entry
 
 
             
-		if len(self.history()) < 100:
-            new_bid_price = bid_prices[0] - self.position * 100 if bid_prices[0] != 0 else 0
-            new_ask_price = ask_prices[0] - self.position * 100 if ask_prices[0] != 0 else 0
+		if len(self.future_history) < 100 or len(self.etf_history):
+			new_bid_price = bid_prices[0] - self.position * 100 if bid_prices[0] != 0 else 0
+			new_ask_price = ask_prices[0] - self.position * 100 if ask_prices[0] != 0 else 0
 
-            if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
-                self.send_cancel_order(self.bid_id)
-                self.bid_id = 0
-            if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
-                self.send_cancel_order(self.ask_id)
-                self.ask_id = 0
+			if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
+				self.send_cancel_order(self.bid_id)
+				self.bid_id = 0
+			if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
+				self.send_cancel_order(self.ask_id)
+				self.ask_id = 0
 
-            if self.bid_id == 0 and new_bid_price != 0 and self.position < 100:
-                self.bid_id = next(self.order_ids)
-                self.bid_price = new_bid_price
-                self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, 1, Lifespan.GOOD_FOR_DAY)
+			if self.bid_id == 0 and new_bid_price != 0 and self.position < 100:
+				self.bid_id = next(self.order_ids)
+				self.bid_price = new_bid_price
+				self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, 1, Lifespan.GOOD_FOR_DAY)
 
-            if self.ask_id == 0 and new_ask_price != 0 and self.position > -100:
-                self.ask_id = next(self.order_ids)
-                self.ask_price = new_ask_price
-                self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, 1, Lifespan.GOOD_FOR_DAY)
+			if self.ask_id == 0 and new_ask_price != 0 and self.position > -100:
+				self.ask_id = next(self.order_ids)
+				self.ask_price = new_ask_price
+				self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, 1, Lifespan.GOOD_FOR_DAY)
 
+		
 		else:
 			if instrument == Instrument.FUTURE:
-				total_before_avg = 0
+
+				total_ask_before_avg = 0
+				total_bid_before_avg = 0
+
 				for i in [0:50]:
-					total_before_avg =  Future_history[sequence_number-i][0]
-				new_bid_price = total_before_avg/50
+					total_ask_before_avg = future_history[sequence_number-i]['ask']
+					total_bid_before_avg = future_history[sequence_number-i]['bid']
+
+				new_ask_price = total_ask_before_avg/50
+				new_bid_price = total_bid_before_avg/50
+
+				self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, 1, Lifespan.KILL_AND_FILL)
+				self.send_insert_order(self.ask_id, Side.SELL, new_bid_price, 1, Lifespan.KILL_AND_FILL)
+				
+
 			elif instrument == Instrument.ETF:
-				total_before_avg = 0
+
+				total_ask_before_avg = 0
+				total_bid_before_avg = 0
+
 				for i in [0:50]:
-					total_before_avg =  Future_history[sequence_number-i][0]
-				new_bid_price = total_before_avg/50
+					total_ask_before_avg = etf_history[sequence_number-i]['ask']
+					total_bid_before_avg = etf_history[sequence_number-i]['bid']
+
+				new_ask_price = total_ask_before_avg/50
+				new_bid_price = total_bid_before_avg/50
+
+				self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, 1, Lifespan.KILL_AND_FILL)
+				self.send_insert_order(self.ask_id, Side.SELL, new_bid_price, 1, Lifespan.KILL_AND_FILL)
+
+
 
     def on_order_status_message(self, client_order_id: int, fill_volume: int, remaining_volume: int, fees: int) -> None:
         """Called when the status of one of your orders changes.
