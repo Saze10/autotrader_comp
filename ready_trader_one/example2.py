cc9d9ab451ff9cb8ea3d94b1cf640985ced0b40e
Hooli0 <andre.calma@yahoo.com>
2020-03-10 22:13:48 +1100

    Initial

--- ready_trader_one/example2.py
+++ ready_trader_one/example2.py
@@ -23,7 +23,6 @@ class AutoTrader(BaseAutoTrader):
         """Called periodically to report the status of an order book."""
         
         if instrument == Instrument.FUTURE:
-            """
             new_bid_price = bid_prices[0] - self.position * 100 if bid_prices[0] != 0 else 0
             new_ask_price = ask_prices[0] - self.position * 100 if ask_prices[0] != 0 else 0
 
@@ -43,7 +42,6 @@ class AutoTrader(BaseAutoTrader):
                 self.ask_id = next(self.order_ids)
                 self.ask_price = new_ask_price
                 self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, 1, Lifespan.GOOD_FOR_DAY)
-            """
 
     def on_order_status_message(self, client_order_id: int, fill_volume: int, remaining_volume: int, fees: int) -> None:
         """Called when the status of one of your orders changes."""
