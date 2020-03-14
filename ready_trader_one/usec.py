21f2aae580cea70c60b602d8512b78f4438a9f5d
Saze10 <saze@protonmail.com>
2020-03-14 15:25:10 +1100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- ready_trader_one/usec.py
+++ ready_trader_one/usec.py
@@ -92,14 +92,15 @@ class AutoTrader(BaseAutoTrader):
 
 
         def check_active_order_position():
-            sum_volumes = 0
+            sum_positive = 0
+            sum_negative = 0
             for key in list(self.active_order_history.keys()):
                 if self.active_order_history[key][4] == Side.BUY:
-                    sum_volumes += self.active_order_history[key][3]
+                    sum_positive += self.active_order_history[key][3]
                 else:
-                    sum_volumes -= self.active_order_history[key][3]
-            self.logger.warning("active order position: %d", sum_volumes + self.position)
-            return sum_volumes + self.position
+                    sum_negative -= self.active_order_history[key][3]
+            self.logger.warning("active order position: %d", sum_positive + sum_negative + self.position)
+            return (sum_positive, sum_negative)
 
                 
         def order_quantity(trader_stance, side):
@@ -115,14 +116,14 @@ class AutoTrader(BaseAutoTrader):
                 #checking for cumulative position of active orders + current volume
                 active_order_position = check_active_order_position()
                 
-                if active_order_position + volume >= 100 and side:
-                    self.logger.warning("order_quantity returns: %d", 100 - active_order_position)
-                    return 100 - active_order_position
-                elif active_order_position - volume <= -100 and not side:
-                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position)
-                    return 100 + active_order_position
+                if active_order_position[0] + volume >= 100 and side:
+                    self.logger.warning("order_quantity returns: %d", 100 - active_order_position[0])
+                    return 100 - active_order_position[0]
+                elif active_order_position[1] - volume <= -100 and not side:
+                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position[1])
+                    return 100 + active_order_position[1]
                 else:
-                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position)
+                    self.logger.warning("order_quantity returns: %d", 100 + volume)
                     return volume
 
 
@@ -134,81 +135,30 @@ class AutoTrader(BaseAutoTrader):
 
                 active_order_position = check_active_order_position()
                 
-                if active_order_position + volume >= 100 and side:
-                    self.logger.warning("order_quantity returns: %d", 100 - active_order_position)
-                    return 100 - active_order_position
-                elif active_order_position - volume <= -100 and not side:
-                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position)
-                    return 100 + active_order_position
+                if active_order_position[0] + volume >= 100 and side:
+                    self.logger.warning("order_quantity returns: %d", 100 - active_order_position[0])
+                    return 100 - active_order_position[0]
+                elif active_order_position[1] - volume <= -100 and not side:
+                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position[1])
+                    return 100 + active_order_position[1]
                 else:
-                    self.logger.warning("order_quantity returns: %d", 100 + active_order_position)
+                    self.logger.warning("order_quantity returns: %d", volume)
                     return volume
 
 
         # Amon's layout is 0: id  1: tick number 2: side 3: price 4: volume
         # Andrew's layout is 0: id  1: tick number 2: price 3: volume 4: side
         # Change amon's things to the equivalent in andrew - should be done now
-        def check_same_price_order(volume, ask_trading_price, bid_trading_price):
+        def check_same_price_order(side, trading_price, order_id, trading_volume):
             self.logger.warning("Checking for same price")
             
-            ask_order_exist = False
-            bid_order_exist = False
-            # Variables for checking if identical opposite side order exists
-            opposite_bid_found = False
-            opposite_ask_found = False
-            ask_order_num_cancel = 0
-            bid_order_num_cancel = 0
-            old_ask_order_volume = 0
-            old_bid_order_volume = 0
-
-            if len(self.active_order_history) != 0:
-                #Checks for identical orders in active order_history
-                for key in list(self.active_order_history.keys()):
-                    if self.active_order_history[key][4] == 0 and self.active_order_history[key][2] == ask_trading_price:
-                        ask_order_num_cancel = key
-                        old_ask_order_volume = self.active_order_history[key][3]
-                        ask_order_exist = True
-                    if self.active_order_history[key][4] == 1 and self.active_order_history[key][2] == ask_trading_price: # If we find a bid of the same price as our ask
-                        opposite_bid_found = True
-                        
-
-                    if self.active_order_history[key][4] == 1 and self.active_order_history[key][2] == bid_trading_price:
-                        bid_order_num_cancel = key
-                        old_bid_order_volume = self.active_order_history[key][3]
-                        bid_order_exist = True
-                    if self.active_order_history[key][4] == 0 and self.active_order_history[key][2] == bid_trading_price: # If we find a ask of the same price as our bid
-                        opposite_add_found = True
-
-                self.logger.warning("Looped through active order history to check price")
+            for key in list(self.active_order_history.keys()):
+                if self.active_order_history[key][4] != side and self.active_order_history[key][2] == trading_price:
+                    self.op_send_cancel_order(self.active_order_history[key][0])
+                    self.op_send_insert_order(order_id, side, trading_price, trading_volume, Lifespan.GOOD_FOR_DAY)
+                    return True
+            return False
                 
-                if not opposite_bid_found:
-                    if ask_order_exist == True:
-                        self.logger.warning("Found same ask price")
-                        if self.op_send_cancel_order(ask_order_num_cancel): # This function returns true if cancel is successful
-                            self.ask_id = next(self.order_ids)
-                            self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume + old_ask_order_volume, Lifespan.GOOD_FOR_DAY)
-                            self.logger.warning("Replaced ask order with new volume")
-                    else:
-                        self.ask_id = next(self.order_ids)
-                        self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
-
-                if not opposite_ask_found:
-                    if bid_order_exist == True:
-                        self.logger.warning("Found same bid price")
-                        if self.op_send_cancel_order(bid_order_num_cancel): # This function returns true if cancel is successful
-                            self.bid_id = next(self.order_ids)
-                            self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume + old_bid_order_volume, Lifespan.GOOD_FOR_DAY)
-                            self.logger.warning("Replaced bid order with new volume")
-                    else:
-                        self.bid_id = next(self.order_ids)
-                        self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
-            else:
-                self.ask_id = next(self.order_ids)
-                self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
-
-                self.bid_id = next(self.order_ids)
-                self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
-
 
 
         
@@ -228,7 +178,10 @@ class AutoTrader(BaseAutoTrader):
                 order_volume = order_quantity(False, False)
                 if order_volume > 0:
                     self.ask_id = next(self.order_ids)
-                    self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
+                    
+                    # function returns True when it replaces an order
+                    if not check_same_price_order(Side.SELL, ask_trading_price, self.ask_id, order_volume):
+                        self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
 
                 # Make a bid at last trade price - ask bid spread
                 bid_trading_price = self.round_to_trade_tick(last_trading_price[0][0] - ask_bid_spread)
@@ -236,7 +189,8 @@ class AutoTrader(BaseAutoTrader):
                 order_volume = order_quantity(False, True)
                 if order_volume > 0:
                     self.bid_id = next(self.order_ids)
-                    self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
+                    if not check_same_price_order(Side.BUY, ask_trading_price, self.ask_id, order_volume):
+                        self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
 
             else: # Passive strategy
 
@@ -247,11 +201,13 @@ class AutoTrader(BaseAutoTrader):
                     order_volume = order_quantity(True, False)
                     if order_volume > 0:
                         self.ask_id = next(self.order_ids)
-                        self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
+                        if not check_same_price_order(Side.SELL, ask_trading_price, self.ask_id, order_volume):
+                            self.op_send_insert_order(self.ask_id, Side.SELL, ask_trading_price, order_volume, Lifespan.GOOD_FOR_DAY)
                     order_volume = order_quantity(True, True)
                     if order_volume > 0:
                         self.bid_id = next(self.order_ids)
-                        self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume , Lifespan.GOOD_FOR_DAY)
+                        if not check_same_price_order(Side.BUY, bid_trading_price, self.bid_id, order_volume):
+                            self.op_send_insert_order(self.bid_id, Side.BUY, bid_trading_price, order_volume , Lifespan.GOOD_FOR_DAY)
 
     def on_order_status_message(self, client_order_id: int, fill_volume: int, remaining_volume: int, fees: int) -> None:
         """Called when the status of one of your orders changes.
@@ -303,8 +259,7 @@ class AutoTrader(BaseAutoTrader):
             temp[1] += 1
             self.active_order_history[key] = tuple(temp)
             if self.active_order_history[key][1] > 3:
-                if self.op_send_cancel_order(key): # This function returns true if cancel is successful
-                    del self.active_order_history[key]
+                self.op_send_cancel_order(key) # This function returns true if cancel is successful
             
                 
         
@@ -372,9 +327,12 @@ class AutoTrader(BaseAutoTrader):
 
     def op_send_cancel_order(self, client_order_id: int) -> None:
         if self.get_projected_op_rate(1) <= 19.5: # Technically should be 20 - setting it stricter for now
-            self.send_cancel_order(client_order_id)
             self.op_history.append(time.time())
+            if client_order_id in self.active_order_history:
+                self.send_cancel_order(client_order_id)
+                del self.active_order_history[client_order_id]
             return True
+
         return False
 
     def op_send_amend_order(self, client_order_id: int, volume: int) -> None:
